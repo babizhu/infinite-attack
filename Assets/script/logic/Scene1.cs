99ad68fcac8beda147b99d1d837daf29c940a231
liukun <liu_kun@msn.com>
2014-11-05 12:00:10 +0800

    继续完善点选功能

---
+++ Assets/script/logic/Scene1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene1 : MonoBehaviour {
+
+    /**
+     * 武器模板列表，10秒后隐藏
+     */
+    public GameObject weaponTempletList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //weaponTempletList.renderer.enable = true;
+        }
+	}
+}
---
+++ Assets/script/weapon/WeaponTempletList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * 控制WeaponTempletList的相关代码
+ */
+public class WeaponTempletList : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void show()
+    {
+
+    }
+
+    public void hide()
+    {
+
+    }
+}
